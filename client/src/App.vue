1b4d2a5f40cae0c5065751c2d296e113c2a23e28
Unique <bin.guo@ostw.com>
2018-09-21 13:09:54 +0800

    Add three.js

--- client/src/App.vue
+++ client/src/App.vue
@@ -49,7 +49,7 @@
                             background-color="#545c64"
                             text-color="#fff"
                             active-text-color="#ffd04b">
-                        <el-menu-item index="1">处理中心</el-menu-item>
+                        <el-menu-item index="1">模型预览</el-menu-item>
                         <el-submenu index="2">
                             <template slot="title">我的工作台</template>
                             <el-menu-item index="2-1">选项1</el-menu-item>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,104 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="ABC" />
+  <div :id="id" class="home" style="position: relative">
+    <div class="container"></div>
   </div>
 </template>
 
+<style>
+  .home{
+    height: 100%;
+    overflow: hidden;
+  }
+
+  .container {
+
+  }
+</style>
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import $ from 'jquery'
+  import * as THREE from 'three';
+  import GLTFLoader  from 'three-gltf-loader'
+  import OrbitControls from 'three-orbitcontrols'
+
+  const scene = new THREE.Scene();
 
 export default {
   name: 'home',
+  data: function () {
+    return {
+      id: 'home'
+    }
+  },
   components: {
-    HelloWorld
+  },
+  mounted: function () {
+    var that = this
+    var scene, camera, pointLight
+    var renderer, mixer, controls
+    var clock = new THREE.Clock()
+    var $container = $('.container')
+
+    var resizeCanvas = function () {
+      let $main = $('.el-main')
+      that.width = $main[0].clientWidth
+      that.height = $main[0].clientHeight
+      renderer.setSize(that.width, that.height)
+    }
+
+
+    renderer = new THREE.WebGLRenderer({
+      antialias: true
+    });
+
+
+    renderer.setPixelRatio(window.devicePixelRatio)
+    resizeCanvas()
+
+    renderer.gammaOutput = true
+    renderer.gammaFactor = 2.2
+    //container.appendChild( renderer.domElement );
+    $container.append(renderer.domElement)
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xbfe3dd);
+    camera = new THREE.PerspectiveCamera(40, that.width / that.height, 1, 1000);
+    camera.position.set(8, 6, -8);
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0.5, 0);
+    controls.enablePan = false;
+    scene.add(new THREE.AmbientLight(0x404040));
+    pointLight = new THREE.PointLight(0xffffff, 1);
+    pointLight.position.copy(camera.position);
+    scene.add(pointLight);
+
+    var loader = new GLTFLoader();
+    loader.load('models/gltf/qtAnimation.glb', function (gltf) {
+      var model = gltf.scene;
+      model.position.set(0, 0, 0);
+      model.scale.set(0.05, 0.05, 0.05);
+      scene.add(model);
+      mixer = new THREE.AnimationMixer(model);
+      console.log(gltf.animations.length)
+      mixer.clipAction(gltf.animations[0]).play();
+      animate();
+    }, undefined, function (e) {
+      console.error(e);
+    })
+
+    window.onresize = function () {
+      resizeCanvas()
+      camera.aspect = that.width / that.height
+      camera.updateProjectionMatrix()
+
+    }
+
+    function animate() {
+      requestAnimationFrame(animate)
+      var delta = clock.getDelta()
+      mixer.update(delta)
+      controls.update(delta)
+      renderer.render(scene, camera)
+    }
   }
 }
 </script>
